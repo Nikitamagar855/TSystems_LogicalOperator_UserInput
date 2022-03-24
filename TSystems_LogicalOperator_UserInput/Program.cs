c41d542ed6cb2cb3d4049be62985df1e903a91c5
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:45:39 +0530

    Add project files.

---
+++ TSystems_LogicalOperator_UserInput/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_LogicalOperator_UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        start:
+
+            Console.WriteLine("Enter First Value True or False");
+            bool a = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Value True or False");
+            bool b = Convert.ToBoolean(Console.ReadLine());
+
+            bool result;
+
+            result = a && b;
+            Console.WriteLine($"The And operator would be\n{result}");
+
+            result = a || b;
+            Console.WriteLine($"The Or operator would be\n{result}");
+
+            result = a != b;
+            Console.WriteLine($"The Not Equal operator would be\n{result}");
+            Console.ReadLine();
+            goto start;
+
+
+
+
+
+        }
+    }
+}
